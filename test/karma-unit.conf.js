3e8183d76e8f09c6e4e4b0943c7e60d752733fba
Ben Boyle <benjamins.boyle@gmail.com>
2014-11-05 11:32:39 +1000

    update angular dependencies
    - local angular libs for testing
    - fix lib paths for karma

--- test/karma-unit.conf.js
+++ test/karma-unit.conf.js
@@ -1,9 +1,10 @@
 module.exports = function(config) {
   config.set({
     files : [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/angular/angular.js',
+      'bower_components/angular-animate/angular-animate.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-mocks/angular-mocks.js',
       'app/scripts/homePages.js',
       'app/scripts/app.js',
       'test/unit/**/*.js'

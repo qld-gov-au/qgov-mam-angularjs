0c570beba34e85c9e54efcd5abcdd9fc52a617ed
Ben Boyle <benjamins.boyle@gmail.com>
2014-11-06 12:18:56 +1000

    add constant for results per page config
    - default results per page to 10
    - fix dependency injection format

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,15 +2,19 @@ angular.module('qgovMam', [ 'ngRoute', 'hc.marked', 'qgov', 'searchView' ])
 // angular.module('qgovMam', [ 'ngRoute', 'qgov', 'searchView' ])
 
 .constant( 'TPL_PATH', '/templates' )
+// search results
+.constant( 'RESULTS_PER_PAGE', 10 )
 
 
 // markdown config
-.config([ 'markedProvider', function( markedProvider ) {
+.config([ 'markedProvider',
+function(  markedProvider ) {
 	markedProvider.setOptions({ gfm: true });
 }])
 
 
-.config(function( $routeProvider, TPL_PATH ) {
+.config([ '$routeProvider', 'TPL_PATH',
+function(  $routeProvider,   TPL_PATH ) {
 	$routeProvider
 	.when( '/', {
 		controller: 'SearchController',
@@ -18,4 +22,4 @@ angular.module('qgovMam', [ 'ngRoute', 'hc.marked', 'qgov', 'searchView' ])
 		templateUrl : TPL_PATH + '/search.html'
 	})
 	.otherwise({ redirectTo : '/' });
-});
+}]);

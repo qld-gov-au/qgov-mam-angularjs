a750648e23c0c5ff37196bf98010f79321e82d15
Ben Boyle <benjamins.boyle@gmail.com>
2014-12-10 00:19:18 +1000

    remove error view controller

--- app/scripts/errorController.js
+++
@@ -1,9 +0,0 @@
-angular.module( 'mam.errorView', [ 'ngRoute' ])
-
-
-.config([ '$routeProvider',
-function(  $routeProvider ) {
-	$routeProvider.when( '/error', {
-		templateUrl: 'error.html'
-	});
-}]);
